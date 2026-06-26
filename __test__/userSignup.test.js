a88baf64c08a38226de7ce6077ca9aa965083b26
Tarek Mohamed <tarek.mohamed105900@gmail.com>
2026-06-26 06:24:06 +0300

    update test file

--- __test__/userSignup.test.js
+++ __test__/userSignup.test.js
@@ -5,7 +5,6 @@ jest.mock("../middleware/sendEmail");
 
 const userModel = require("../model/user.model");
 const userSignUpFun = require("../module/user/controller/user.controller");
-
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const sendEmail = require("../middleware/sendEmail");
@@ -13,54 +12,54 @@ const sendEmail = require("../middleware/sendEmail");
 test("user signup success", async () => {
   const req = {
     body: {
-      name: "Tarek",
+      name: "Tarek Mohamed",
       email: "tarek@test.com",
-      password: "123456",
+      password: "tarek@123",
     },
   };
-
   const res = {
     status: jest.fn().mockReturnThis(),
     json: jest.fn(),
   };
 
   userModel.findOne.mockResolvedValue(null);
-
-  jwt.sign.mockReturnValue("my_fake_token_string");
-
+  jwt.sign.mockReturnValue("fake_string_token");
   sendEmail.mockResolvedValue(true);
-
   bcrypt.hash.mockImplementation((password, salt, callback) => {
-    callback(null, "hashed-password");
+    callback(null, "hashed_password");
   });
 
   userModel.insertMany.mockResolvedValue({
-    name: "Tarek",
+    name: "Tarek Mohamed",
     email: "tarek@test.com",
-    password: "hashed-password",
+    password: "hashed_password",
   });
-
   await userSignUpFun(req, res);
   expect(res.status).toHaveBeenCalledWith(201);
-
   expect(res.json).toHaveBeenCalledWith({
     message: "success",
     registerUser: {
-      name: "Tarek",
+      name: "Tarek Mohamed",
       email: "tarek@test.com",
-      password: "hashed-password",
+      password: "hashed_password",
     },
   });
-
   expect(userModel.findOne).toHaveBeenCalledWith({
     email: "tarek@test.com",
   });
-
-  expect(jwt.sign).toHaveBeenCalled();
-
-  expect(sendEmail).toHaveBeenCalled();
-
-  expect(bcrypt.hash).toHaveBeenCalled();
-
-  expect(userModel.insertMany).toHaveBeenCalled();
+  expect(jwt.sign).toHaveBeenCalledWith(
+    { email: "tarek@test.com" },
+    process.env.PRIVATE_KEY,
+  );
+  expect(sendEmail).toHaveBeenCalledWith("tarek@test.com", expect.any(String));
+  expect(bcrypt.hash).toHaveBeenCalledWith(
+    "tarek@123",
+    10,
+    expect.any(Function),
+  );
+  expect(userModel.insertMany).toHaveBeenCalledWith({
+    name: "Tarek Mohamed",
+    email: "tarek@test.com",
+    password: "hashed_password",
+  });
 });
